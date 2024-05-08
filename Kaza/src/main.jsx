11bc650d8486c4104092be362e476127cfaf94ee
guiback0 <guillaume.zimmer.pro@gmail.com>
2024-05-08 17:02:48 +0200

    react router dom

--- Kaza/src/main.jsx
+++ Kaza/src/main.jsx
@@ -1,10 +1,47 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+/* import "./index.css"; */
+import { createBrowserRouter, NavLink, RouterProvider } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const router = createBrowserRouter([
+   {
+      path: "/",
+      element: (
+         <div>
+            Main
+            <nav>
+               <NavLink to="/">Main</NavLink>
+               <NavLink to="/about">A propos</NavLink>
+            </nav>
+         </div>
+      ),
+      children: [
+         {
+            path: "/about",
+            element: (
+               <div>
+                  About
+                  <nav>
+                     <NavLink to="/">Main</NavLink>
+                     <NavLink to="/about">A propos</NavLink>
+                  </nav>
+               </div>
+            ),
+         },
+         /* {
+           path: "/logement/:id",
+           element: <Logement />,
+        }, */
+         {
+            path: "*",
+            element: <div>404</div>,
+         },
+      ],
+   },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+   <React.StrictMode>
+      <RouterProvider router={router} />
+   </React.StrictMode>
+);
