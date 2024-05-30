457017d4e7a72dab43d45062d2253cd493dd1889
guiback0 <guillaume.zimmer.pro@gmail.com>
2024-05-30 09:45:07 +0200

    arrow with fontawesome

--- Kaza/src/components/dropdown/Dropdown.jsx
+++ Kaza/src/components/dropdown/Dropdown.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import PropTypes from "prop-types";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
 import "./dropdown.scss";
 
 export default function Dropdown({ title, text }) {
@@ -13,17 +15,10 @@ export default function Dropdown({ title, text }) {
       <div className="dropdown">
          <button className="dropdown-toggle" onClick={toggleDropdown}>
             <h2>{title}</h2>
-            <svg
+            <FontAwesomeIcon
                className={`${isOpen ? "rotate-arrow" : ""}`}
-               width="16"
-               viewBox="0 0 32 33"
-               fill="none"
-               xmlns="http://www.w3.org/2000/svg">
-               <path
-                  d="M17.2103 21.8522C16.5409 22.5216 15.4538 22.5216 14.7843 21.8522L4.50206 11.5699C3.83265 10.9005 3.83265 9.81332 4.50206 9.1439C5.17148 8.47448 6.25862 8.47448 6.92804 9.1439L16 18.2159L25.072 9.14926C25.7414 8.47984 26.8285 8.47984 27.4979 9.14926C28.1674 9.81868 28.1674 10.9058 27.4979 11.5752L17.2157 21.8575L17.2103 21.8522Z"
-                  fill="white"
-               />
-            </svg>
+               icon={faChevronDown}
+            />
          </button>
          <ul className={`dropdown-content ${isOpen ? "dropdown-open" : ""}`}>
             {Array.isArray(text) ? (
