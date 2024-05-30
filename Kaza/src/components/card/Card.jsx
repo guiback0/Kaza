cb4c7a553aba333d6ff342fb1e22fac4647b17bb
guiback0 <guillaume.zimmer.pro@gmail.com>
2024-05-30 14:32:38 +0200

    cards style

--- Kaza/src/components/card/Card.jsx
+++ Kaza/src/components/card/Card.jsx
@@ -4,24 +4,21 @@ import "./card.scss";
 
 export default function Card({ id, title, cover }) {
    const cardStyle = {
-      background: `url(${cover})`,
-      backgroundSize: "cover",
-      backgroundPosition: "center",
+      background: `url(${cover}) no-repeat center/cover`,
+      position: "relative",
    };
+
    return (
-      <>
-         <Link to={`/logement/${id}`}>
-            <div className="card" style={cardStyle}>
-               <h3 className="cardTitle">{title}</h3>
-            </div>
-         </Link>
-      </>
+      <Link to={`/logement/${id}`}>
+         <div className="card" style={cardStyle}>
+            <h3 className="cardTitle">{title}</h3>
+         </div>
+      </Link>
    );
 }
 
-// Définition des PropTypes pour la validation des props
 Card.propTypes = {
    id: PropTypes.string.isRequired,
    title: PropTypes.string.isRequired,
    cover: PropTypes.string.isRequired,
- };
+};
