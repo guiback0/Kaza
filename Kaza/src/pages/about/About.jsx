92c4893ccf0702f733e439b2281d6b97f04fd0b2
guiback0 <guillaume.zimmer.pro@gmail.com>
2024-05-31 23:30:50 +0200

    responsiv 2

--- Kaza/src/pages/about/About.jsx
+++ Kaza/src/pages/about/About.jsx
@@ -6,22 +6,24 @@ export default function About() {
    return (
       <div className="pagesContainer">
          <AboutBanner />
-         <Dropdown
-            title="Fiabilité"
-            text="Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulierement vérifiées par nos équipes."
-         />
-         <Dropdown
-            title="Respect"
-            text="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entrainera une exclusion de notre plateforme."
-         />
-         <Dropdown
-            title="Service"
-            text="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entrainera une exclusion de notre plateforme."
-         />
-         <Dropdown
-            title="Sécurité"
-            text="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."
-         />
+         <div className="dropdownContainer">
+            <Dropdown
+               title="Fiabilité"
+               text="Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulierement vérifiées par nos équipes."
+            />
+            <Dropdown
+               title="Respect"
+               text="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entrainera une exclusion de notre plateforme."
+            />
+            <Dropdown
+               title="Service"
+               text="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entrainera une exclusion de notre plateforme."
+            />
+            <Dropdown
+               title="Sécurité"
+               text="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."
+            />
+         </div>
       </div>
    );
 }
