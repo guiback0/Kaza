0496a47f03c5f2ccbcc7f79a79e1d8f18ac9ad75
guiback0 <guillaume.zimmer.pro@gmail.com>
2024-06-06 16:18:55 +0200

    underline navlink

--- Kaza/src/components/header/Header.jsx
+++ Kaza/src/components/header/Header.jsx
@@ -1,12 +1,15 @@
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
+import { useLocation } from "react-router-dom";
 import "./header.scss";
 
 export default function Header({ navigation, headerLogo, imageAlt }) {
+   const location = useLocation();
+
    return (
       <header className="header">
          <div className="header__logo">
-            <Link to={"/"}>
+            <Link to={"/"} className="header__nav-link">
                <img
                   className="header__logo-img"
                   src={headerLogo}
@@ -18,7 +21,12 @@ export default function Header({ navigation, headerLogo, imageAlt }) {
             <ul className="header__nav-list">
                {navigation.map((item) => (
                   <li className="header__nav-item" key={item.name}>
-                     <Link to={item.path} className="header__nav-link">
+                     <Link
+                        to={item.path}
+                        className="header__nav-link"
+                        aria-current={
+                           item.path === location.pathname ? "page" : undefined
+                        }>
                         <span>{item.name}</span>
                      </Link>
                   </li>
